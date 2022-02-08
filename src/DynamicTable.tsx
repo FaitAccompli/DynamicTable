1c4a178a0fa65798f2667499949ef6b2f2622894
Montesclaros, Ray Mart <ray.mart.montesclaros@nordicsemi.no>
2022-02-08 20:50:59 +0800

    added files

---
+++ src/DynamicTable.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+interface State {
+	message: string;
+	items: Array<string>;
+}
+export default class DynamicTable extends React.Component<any, State> {
+	constructor(props: any) {
+		super(props);
+
+		this.state = {
+			message: "",
+			items: [],
+		};
+	}
+
+	updateMessage(event: any) {
+		this.setState({
+			message: event.target.value,
+		});
+	}
+
+	handleClick() {
+		var items = this.state.items;
+
+		items.push(this.state.message);
+
+		this.setState({
+			items: items,
+			message: "",
+		});
+	}
+
+	handleItemChanged(i: number, event: any) {
+		var items = this.state.items;
+		items[i] = event.target.value;
+
+		this.setState({
+			items: items,
+		});
+	}
+
+	handleItemDeleted(i: any) {
+		var items = this.state.items;
+
+		items.splice(i, 1);
+
+		this.setState({
+			items: items,
+		});
+	}
+
+	renderRows() {
+		var context = this;
+
+		return this.state.items.map(function (o, i) {
+			return (
+				<tr key={"item-" + i}>
+					<td>
+						<input
+							type="text"
+							value={o}
+							onChange={context.handleItemChanged.bind(context, i)}
+						/>
+					</td>
+					<td>
+						<button onClick={context.handleItemDeleted.bind(context, i)}>
+							Delete
+						</button>
+					</td>
+				</tr>
+			);
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<table className="">
+					<thead>
+						<tr>
+							<th>Item</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>{this.renderRows()}</tbody>
+				</table>
+				<hr />
+				<input
+					type="text"
+					value={this.state.message}
+					onChange={this.updateMessage.bind(this)}
+				/>
+				<button onClick={this.handleClick.bind(this)}>Add Item</button>
+			</div>
+		);
+	}
+}
